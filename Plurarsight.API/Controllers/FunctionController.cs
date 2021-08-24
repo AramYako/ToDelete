3de02a2144d5b3508a913c062e0e9aadb1cf455b
Aram Yako <aramyako@hotmail.com>
2021-08-24 22:31:42 +0200

    first commit

---
+++ Plurarsight.API/Controllers/FunctionController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plurarsight.API.Controllers
+{
+    namespace Plurarsight.API.Controllers
+    {
+        [ApiController]
+        [Route("[controller]")]
+        public class FunctionController : ControllerBase
+        {
+            private static readonly List<string> Functions = new List<string>
+        {
+            "Get", "Post"
+            };
+
+
+
+            [HttpGet]
+            public ActionResult<IEnumerable<string>> Get()
+            {
+                return Ok(Functions);
+            }
+
+            [HttpPost]
+            public ActionResult<IEnumerable<string>> Create(string function)
+            {
+                Functions.Add(function);
+
+                return Ok(function);
+            }
+        }
+    }
+}
---
+++ Plurarsight.API/Controllers/NameController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plurarsight.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class NameController : ControllerBase
+    {
+        private static readonly List<string> Names = new List<string>
+        {
+            "Aram", "AramOne", "AramTwo"
+        };
+
+  
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return Ok(Names);
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<string>> CreateName(string name)
+        {
+            Names.Add(name);
+
+            return Ok(name);
+        }
+    }
+}
